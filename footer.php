3a7f95cd922d3a92cdc3807ae2d8988dd4b96e07
Robert Hampton <robbzilla@byui.edu>
2020-07-21 20:06:46 -0500

    index

---
+++ footer.php
@@ -0,0 +1,5 @@
+<?php
+    echo '<div class="jumbotron text-center" style="margin-bottom:0">';
+    echo '<p>thanks for visiting</p>';
+    echo '</div>';
+?>
---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+echo '<head>';
+echo '<title>Robert Hampton Assignments</title>';
+echo '<meta charset="utf-8">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
+echo '<link rel="stylesheet" href="style.css">';
+echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">';
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>';
+echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>';
+echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>';
+echo '</head>';
+echo '<div class="jumbotron text-center" style="margin-bottom:0">';
+echo '<h1>Robert Hampton</h1>';
+echo '<p>assignments.</p>';
+echo '</div>';
+echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">';
+echo '<ul class="navbar-nav mx-auto">';
+echo '<li class="nav-item">';
+echo '<a class="nav-link" href="#">Gallery</a>';
+echo '</li>';
+echo '</ul>';
+echo '</nav>';
+echo '<br>';
+echo '</div>';
+?>
